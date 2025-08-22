c691b07a3e60748fc756aa33c4085a3788c9f3f7
gitchinmoydas <chinmoychdas@gmail.com>
2025-08-22 12:57:58 +0530

    first commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import {ENV} from './env.js';
+
+export const connectDB = async () => {
+
+    try{
+
+       const conn= await mongoose.connect(ENV.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    }catch(err){
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1);
+    }
+}
