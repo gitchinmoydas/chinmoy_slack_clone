8615d49ad7c04e7ba3e8c32fbc3deeffa48e0cf7
gitchinmoydas <chinmoychdas@gmail.com>
2025-08-23 21:22:31 +0530

    stream and santry

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,11 @@
+import { generateStreamToken } from "../config/inngest.js";
+
+export const getStreamToken = (req, res) => {
+    try {
+        const token = generateStreamToken(req.auth().userId);
+        res.status(200).json({ token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Failed to generate token" });
+    }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,34 @@
+import '../instrument.mjs'
 import express from 'express'
-import {ENV} from './config/env.js'
+import { ENV } from './config/env.js'
 import { connectDB } from './config/db.js';
-import {clerkMiddleware} from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express'
 import { functions, inngest } from './config/inngest.js';
 import { serve } from "inngest/express";
+import chatRoutes from './routes/chat.route.js';
+import * as Sentry from "@sentry/node";
 const app = express();
 app.use(clerkMiddleware())
 app.use(express.json())
+app.get(' ', (req, res) => {
+  throw new Error("My first Sentry error!")
+})
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+Sentry.setupExpressErrorHandler(app);
 
-const startServer=()=>{
+const startServer = () => {
   try {
-      connectDB()
-      if(ENV.NODE_ENV !== "production"){
-        app.listen(ENV.PORT,()=>{
-          console.log('Server is running on port ' + ENV.PORT)
-        })
-      }
+    connectDB()
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log('Server is running on port ' + ENV.PORT)
+      })
     }
+  }
   catch (error) {
     console.error('Error starting server:', error)
     process.exit(1);
