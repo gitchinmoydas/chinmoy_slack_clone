d032e4a3153895fd053a078e021d035b3df16e97
gitchinmoydas <chinmoychdas@gmail.com>
2025-08-26 20:10:53 +0530

    AuthProvider

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,7 @@
+// https://slack-backend-rho.vercel.app/api
+import axios from "axios";
+const BASE_URL= import.meta.env.MODE==="development"?"http://localhost:3000/api": "https://slack-backend-rho.vercel.app/api"
+export const axiosInstance= axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true
+});
